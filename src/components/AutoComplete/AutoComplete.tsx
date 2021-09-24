2e0262249c83d7543eef99eb086e2604a0b8fd2d
wuhuiluo <510426343@qq.com>
2021-09-24 10:02:41 +0800

    latest commit

--- src/components/AutoComplete/AutoComplete.tsx
+++ src/components/AutoComplete/AutoComplete.tsx
@@ -4,7 +4,10 @@ import React, {
   ChangeEvent,
   ReactElement,
   useEffect,
+  KeyboardEvent,
+  useRef,
 } from "react";
+import className from "classnames";
 import Input, { InputProps } from "../Input/input";
 import Icon from "../Icon/icon";
 import useDebounce from "../../hooks/useDebounce";
@@ -32,9 +35,12 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
   const [inputValue, setInputValue] = useState(value as string);
   const [suggestions, setSuggestions] = useState<DataSourceType[]>([]);
   const [loading, setLoading] = useState(false);
+  const [highLightIndex, sethighLightIndex] = useState(-1);
+  const triggerSearch = useRef(false);
   const debounceValue = useDebounce(inputValue, 500);
+
   useEffect(() => {
-    if (debounceValue) {
+    if (debounceValue && triggerSearch) {
       const results = fetchSuggestions(debounceValue);
       if (results instanceof Promise) {
         setLoading(true);
@@ -48,15 +54,49 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
     } else {
       setSuggestions([]);
     }
+    sethighLightIndex(-1);
   }, [debounceValue]);
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value.trim();
     setInputValue(value);
+    triggerSearch.current = true;
   };
   const handleSelect = (item: DataSourceType) => {
     setInputValue(item.value);
     setSuggestions([]);
     onSelect && onSelect(item);
+    triggerSearch.current = false;
+  };
+  const highlight = (index: number) => {
+    if (index <= 0) {
+      index = 0;
+    }
+    if (index >= suggestions.length) {
+      index = suggestions.length - 1;
+    }
+    sethighLightIndex(index);
+  };
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    switch (e.keyCode) {
+      // 回车
+      case 13:
+        if (suggestions[highLightIndex]) {
+          handleSelect(suggestions[highLightIndex]);
+        }
+        break;
+      // 向上箭头
+      case 38:
+        highlight(highLightIndex - 1);
+        break;
+      // 向下箭头
+      case 40:
+        highlight(highLightIndex + 1);
+        break;
+      // esc
+      case 27:
+        setSuggestions([]);
+        break;
+    }
   };
   const renderTemplate = (item: DataSourceType) => {
     return renderOption ? renderOption(item) : item.value;
@@ -65,8 +105,15 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
     return (
       <ul>
         {suggestions.map((item, index) => {
+          const classes = className("in-suggstions-item", {
+            "lin-suggstions-active": index === highLightIndex,
+          });
           return (
-            <li onClick={() => handleSelect(item)} key={index}>
+            <li
+              className={classes}
+              onClick={() => handleSelect(item)}
+              key={index}
+            >
               {renderTemplate(item)}
             </li>
           );
@@ -76,7 +123,12 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
   };
   return (
     <div className="luo-autu-complete">
-      <Input onChange={handleChange} value={inputValue} {...restProps} />
+      <Input
+        onKeyDown={handleKeyDown}
+        onChange={handleChange}
+        value={inputValue}
+        {...restProps}
+      />
       {loading && (
         <ul>
           <Icon icon="spinner" spin />
